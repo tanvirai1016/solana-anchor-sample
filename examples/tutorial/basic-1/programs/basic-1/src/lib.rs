e39552445aa9b4c5c6cb7f5e98632e5a074c5ccc
tanvirai1016 <tanvirai1016@gmail.com>
2022-10-19 21:09:05 +0530

    commit3

--- examples/tutorial/basic-1/programs/basic-1/src/lib.rs
+++ examples/tutorial/basic-1/programs/basic-1/src/lib.rs
@@ -1,22 +1,34 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("F41aWthQJGRVGiCQSHLUnREGd9YGJAFmgiBAicohnbjP");
 
 #[program]
 mod basic_1 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>, data: u64) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         let my_account = &mut ctx.accounts.my_account;
-        my_account.data = data;
+        my_account.data = 0;
         Ok(())
     }
-
+  
     pub fn update(ctx: Context<Update>, data: u64) -> Result<()> {
         let my_account = &mut ctx.accounts.my_account;
         my_account.data = data;
         Ok(())
     }
+
+    pub fn increment(ctx: Context<increment>) -> Result<()> {
+        let my_account = &mut ctx.accounts.my_account;
+        my_account.data += 1;
+        Ok(())
+    }
+    pub fn decrement(ctx: Context<decrement>) -> Result<()> {
+        let my_account = &mut ctx.accounts.my_account;
+        my_account.data -= 1;
+        Ok(())
+    }
+
 }
 
 #[derive(Accounts)]
@@ -30,6 +42,17 @@ pub struct Initialize<'info> {
 
 #[derive(Accounts)]
 pub struct Update<'info> {
+    #[account(mut)]
+    pub my_account: Account<'info, MyAccount>,
+}
+
+#[derive(Accounts)]
+pub struct increment<'info> {
+    #[account(mut)]
+    pub my_account: Account<'info, MyAccount>,
+}
+#[derive(Accounts)]
+pub struct decrement<'info> {
     #[account(mut)]
     pub my_account: Account<'info, MyAccount>,
 }
